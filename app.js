a5b488327a10221de1be1a393f69ae9fd3dc54c7
tiago.c.v17@gmail.com <tiago.c.v17@gmail.com>
2018-05-06 20:06:27 -0400

    port fix

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 
 //const port = 3000;
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 // parse income body middleware
 app.use(bodyParser.json())
