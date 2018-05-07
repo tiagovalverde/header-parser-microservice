387bdecbc05b85b272d7bd8dce91f2bfd9a20046
tiago.c.v17@gmail.com <tiago.c.v17@gmail.com>
2018-05-06 20:49:52 -0400

    response object typo

--- app.js
+++ app.js
@@ -1,38 +1,29 @@
 
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const app = express();
 
-//const port = 3000;
 const port = process.env.PORT || 3000;
 
-// parse income body middleware
-app.use(bodyParser.json())
-app.use(cors());
-
-// routes go here
-// redirect any request to only valid url path
+// routes
 app.get('/api/whoami',(req, res, next) => {
 
-    var software = req.headers['user-agent'].split(') ')[0].split(' (')[1]
+    var software = req.headers['user-agent'].split(') ')[0].split(' (')[1];
     let language = req.headers['accept-language'].split(",")[0];
-    let ipAddress = req.headers['x-forwarded-for'] || 
+    let ipaddress = req.headers['x-forwarded-for'] || 
              req.connection.remoteAddress || 
              req.socket.remoteAddress ||
              req.connection.socket.remoteAddress;
 
     browserInfo = {
-        ipAddress,
+        ipaddress,
         language,
         software
     }
     res.json(browserInfo);
 });
 
-// redirect invalid requests 
+// invalid requests 
 app.get('*', (req, res) => {
-    //res.send('Invalid request')
     res.json({
         sucess: false,
         message: 'Invalid request'
@@ -41,5 +32,4 @@ app.get('*', (req, res) => {
 
 app.listen(port, () => {
     console.log('Server started on port:' + port);
-    
 });
