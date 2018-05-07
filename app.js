8dbf531ee963c618f683ade9a8b25a1578ffafa8
tiago.c.v17@gmail.com <tiago.c.v17@gmail.com>
2018-05-06 20:17:36 -0400

    ip address fix

--- app.js
+++ app.js
@@ -15,20 +15,15 @@ app.use(cors());
 // redirect any request to only valid url path
 app.get('/api/whoami',(req, res, next) => {
 
-    // software
-    let str_userAgent = req.headers['user-agent'];
-    var software = str_userAgent.substring( str_userAgent.indexOf( '(' ) + 1, str_userAgent.indexOf( ')' ) );
-
-    // language
+    var software = req.headers['user-agent'].split(') ')[0].split(' (')[1]
     let language = req.headers['accept-language'].split(",")[0];
-    console.log(language);
-
-    // ip address
-    var remoteAddress = req.connection.remoteAddress;
-    console.log(remoteAddress);
+    let ipAddress = req.headers['x-forwarded-for'] || 
+             req.connection.remoteAddress || 
+             req.socket.remoteAddress ||
+             req.connection.socket.remoteAddress;
 
     browserInfo = {
-        ipaddress: remoteAddress,
+        ipAddress,
         language,
         software
     }
